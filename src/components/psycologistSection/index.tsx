0cf02c1ebac55e06c67cb617eaeba00606e319b4
Samuel Carvalho <carvalho.asamuel@gmail.com>
2025-06-19 17:51:23 -0300

    fix: psycologistSection

--- src/components/psycologistSection/index.tsx
+++ src/components/psycologistSection/index.tsx
@@ -13,21 +13,23 @@ export function PsycologistSection({
   const length = paragraphs.length
   const middle = Math.ceil(length / 2)
   return (
-    <section className='relative flex flex-row w-full overflow-hidden bg-[#A96058] text-white'>
-      <div className='relative z-20 container mx-8 flex flex-col md:flex-row gap-12 px-6 py-32'>
-        <div className='flex flex-col text-center space-y-4 md:w-1/3'>
+    <section id='como-funciona'
+      className='relative flex flex-row w-full h-full lg:max-h-[600px] bg-[#A96058] text-white'
+    >
+      <div className='relative z-20 container mx-8 flex flex-col lg:flex-row gap-12 px-6 py-32'>
+        <div className='flex flex-col text-center space-y-4 w-full lg:w-1/3'>
           <h2 className='text-3xl text-[#EFE6DF] md:text-4xl'>
             {title}
           </h2>
         </div>
-        <div className='space-y-4 text-[#EFE6DF] md:w-1/2'>
+        <div className='space-y-4 text-[#EFE6DF] w-full lg:w-1/2'>
           {paragraphs.slice(0, middle).map((paragraph, index) => (
             <p key={index} className='text-xl'>
               {paragraph}
             </p>
           ))}
         </div>
-        <div className='space-y-4 text-[#EFE6DF] md:w-1/2'>
+        <div className='space-y-4 text-[#EFE6DF] w-full lg:w-1/2'>
           {paragraphs.slice(middle, length).map((paragraph, index) => (
             <p key={index} className='text-xl'>
               {paragraph}
