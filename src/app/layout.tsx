3ffce1d0825362f2deedffde6e3de3600a0211eb
Samuel Carvalho <carvalho.asamuel@gmail.com>
2025-06-21 13:34:56 -0300

    chore: add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import './globals.css'
 import { Header } from '@/components/common/header'
 import { Footer2 } from '@/components/common/footer'
 import { Powered } from '@/components/common/powered'
+import Head from 'next/head'
 
 const montserratSan = Montserrat({
   variable: '--font-montserrat',
@@ -42,12 +43,33 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='pt-br'>
-      <head>
+      <Head>
+        {/* Favicon tradicional */}
+        <link rel="icon" type="image/png" sizes="512x512" href="/favicons/favicon-512x512.png" />
+        <link rel="icon" type="image/png" sizes="256x256" href="/favicons/favicon-256x256.png" />
+        <link rel="icon" type="image/png" sizes="192x192" href="/favicons/favicon-192x192.png" />
+        <link rel="icon" type="image/png" sizes="152x152" href="/favicons/favicon-152x152.png" />
+        <link rel="icon" type="image/png" sizes="128x128" href="/favicons/favicon-128x128.png" />
+        <link rel="icon" type="image/png" sizes="96x96" href="/favicons/favicon-96x96.png" />
+        <link rel="icon" type="image/png" sizes="64x64" href="/favicons/favicon-64x64.png" />
+        <link rel="icon" type="image/png" sizes="48x48" href="/favicons/favicon-48x48.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
+        <link rel="shortcut icon" href="/favicons/favicon.ico" />
+
+        {/* Apple Touch Icon */}
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicons/favicon-180x180.png" />
+
+        {/* Android/Chrome */}
+        <link rel="manifest" href="/favicons/site.webmanifest" />
+
+        {/* Meta para tema do navegador */}
+        <meta name="theme-color" content="#ffffff" />
         <link
           rel='stylesheet'
           href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css'
         />
-      </head>
+      </Head>
       <body
         className={`${montserratSan.variable} ${firaCode.variable} `}
       >
