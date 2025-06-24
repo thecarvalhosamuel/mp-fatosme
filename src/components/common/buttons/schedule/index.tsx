3ef306d5c1c0638370d4dcb001598b45b42dd0f8
Samuel Carvalho <carvalho.asamuel@gmail.com>
2025-06-24 20:54:39 -0300

    fix: scheduleButton

--- src/components/common/buttons/schedule/index.tsx
+++ src/components/common/buttons/schedule/index.tsx
@@ -1,12 +1,18 @@
+'use client'
 import { Tooltip, TooltipContent, TooltipTrigger } from '../../ui/tooltip'
 import { sendWhatsAppMessage } from '@/utils/whatsapp'
 
 export function ScheduleButton() {
+  const handleClick = () => {
+    sendWhatsAppMessage()
+  }
+
   return (
     <Tooltip>
       <TooltipTrigger asChild>
         <div id='agendar' className='flex items-center justify-center'>
-          <span onClick={sendWhatsAppMessage}
+          <span
+            onClick={handleClick}
             className='bg-primary text-xl hover:bg-accent-hover rounded-lg px-6 py-2 text-white transition-colors cursor-pointer'
           >
             Agendar consulta
