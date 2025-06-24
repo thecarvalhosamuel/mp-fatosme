4846f8fe846391ac8a687083314b6c20d45e96a0
Samuel Carvalho <carvalho.asamuel@gmail.com>
2025-06-24 20:46:28 -0300

    fix: create default message

--- src/utils/whatsapp.ts
+++ src/utils/whatsapp.ts
@@ -1,7 +1,6 @@
 import { env } from "@/config/env";
 
-export function sendWhatsAppMessage(){
-  const message = "Olá, gostaria de agendar uma consulta.";
+export function sendWhatsAppMessage(message: string = "Olá, gostaria de agendar uma consulta.") {
   const phoneNumber = env.WHATSAPP
   // Formata o número e a mensagem para que possam ser usados na URL
   const formattedMessage = encodeURIComponent(message); // Ex: Olá, preciso de ajuda
@@ -10,7 +9,7 @@ export function sendWhatsAppMessage(){
   const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
 
   // URL para envio de mensagem no WhatsApp
-  const whatsappUrl = isMobile 
+  const whatsappUrl = isMobile
     ? `whatsapp://send?phone=${phoneNumber}&text=${formattedMessage}` // Tenta abrir o app do WhatsApp no celular
     : `https://wa.me/${phoneNumber}?text=${formattedMessage}`; // Usa o WhatsApp Web se for desktop
 
